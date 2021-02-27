630475ee3ae5ad4f08f731fa9283182996957853
kebal7 <69685391+kebal7@users.noreply.github.com>
2021-02-27 18:09:04 +0545

    scene

---
+++ CreateDarkness/Assets/DoorLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLogic : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _animator.SetBool("opendoor", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            _animator.SetBool("opendoor", false);
+        }
+    }
+}
---
+++ CreateDarkness/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public LayerMask groundMask;
+
+
+    [SerializeField]private CharacterController controller;
+    
+
+    [SerializeField]private Transform cam, groundCheck;
+
+    [SerializeField]private float gravity = -9.81f;
+    [SerializeField]private float groundDistance = 0.4f;
+    
+    [SerializeField]private float speed = 6f;
+    [SerializeField]private float jumpHeight = 3f;
+    
+    [SerializeField]private float turnSmoothTime = 0.1f;
+    
+  
+    private float turnSmoothVelocity;
+    private Vector3 velocity;
+    private bool isGrounded;
+   
+    
+
+ 
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y <0 )
+        {
+            velocity.y = -4f; 
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
