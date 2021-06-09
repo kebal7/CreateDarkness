3c520279818c656b38a249bf57e136b54f5dd110
kebal7 <69685391+kebal7@users.noreply.github.com>
2021-06-09 13:37:10 +0545

    jnbmjb

---
+++ CreateDarkness/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+        void Start()
+    {
+        DisableCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DisableCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void EnableCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
